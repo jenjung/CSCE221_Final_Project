3139fbc8f240b2c9906543b285abd21c463e44a7
jenjung <90529626+jenjung@users.noreply.github.com>
2022-05-01 18:52:24 -0500

    2
    Change 2 by Unnie

--- graph.h
+++ graph.h
@@ -10,85 +10,104 @@
 
 class Graph {
 private:
-    struct edge{
+    // TODO(student): implement graph
+    struct edge {
         size_t source;
         size_t destination;
         double weight;
 
-        edge(size_t src, size_t dest, double w): source(src), destination(dest), weight(w){
-
-        }
+        edge (size_t s, size_t d, double w) : source(s), destination(d), weight(w) {}
     };
-    // TODO(student): implement graph
-    struct vertex{
-        size_t id;
+
+    struct vertex {
+        bool known;
+        size_t distance;
         std::list<edge> connectedEdges;
 
-        vertex(std::list<edge> ce, size_t ID): id(ID), connectedEdges(ce){
+        size_t id;
+        vertex * path;
 
+        vertex (std::list<edge> t = {}, size_t i = 0) : known(false), distance(), path(), id(i), connectedEdges() {
+            for (auto & ele : t) {
+                connectedEdges.push_back(ele);
+            }
         }
+
+        vertex (const vertex & v) : known(v.known), distance(v.distance), path(v.path), id(v.id), connectedEdges(v.connectedEdges) {};
+
+        vertex & operator = (const vertex & v) {
+            known = v.known;
+            distance = v.distance;
+            path = v.path;
+            id = v.id;
+            for (edge e : v.connectedEdges) {
+                connectedEdges.push_back(e);
+            }
+            return * this;
+        } 
     };
+
     size_t edgeCount;
-    std::vector<vertex*> vertex_list;
+    std::vector<vertex*> vertexList;
 
 public:
     // Task 1
-    Graph(): edgeCount(0), vertex_list(0){
-
+    Graph() : vertexList(), edgeCount(0) {};
+    Graph(const Graph& other) : vertexList(), edgeCount(other.edgeCount) {
+        for (vertex * v : other.vertexList) {
+            vertexList.push_back(v);
+        }
     };
-
-    Graph(const Graph& other){
-        edgeCount=other.edgeCount;
-        vertex_list=other.vertex_list;
-    };
-
-    Graph& operator=(const Graph& other){
-        if(this!=&other){
-            edgeCount=other.edgeCount;
-            vertex_list=other.vertex_list;
+    Graph& operator=(const Graph& other) {
+        if (&other == this) {
+            return *this;
+        }
+        edgeCount = other.edgeCount;
+        vertex * temp;
+        for (vertex * v : other.vertexList) {
+            temp = new vertex(v->connectedEdges, v->id);
+            vertexList.push_back(temp);
         }
         return *this;
     };
-
-    ~Graph(){
-        edgeCount=0;
+    ~Graph() {
+        for (vertex * v : vertexList) {
+            delete v;
+        }
+        edgeCount = 0;
     };
 
-    size_t vertex_count() const{
-        return vertex_list.size();
+    size_t vertex_count() const {
+        return vertexList.size();
     };
-
-    size_t edge_count() const{
+    size_t edge_count() const {
         return edgeCount;
     };
 
-    bool contains_vertex(size_t id) const{
-        for(auto& v : vertex_list){
-            if(v->id==id){
+    bool contains_vertex(size_t id) const {
+        for (vertex * v : vertexList) {
+            if (v->id == id) {
                 return true;
             }
         }
         return false;
     };
-
-    bool contains_edge(size_t src, size_t dest) const{
-        for(auto& v : vertex_list){
-            for(auto& e : v->connectedEdges){
-                if(e.source==src&&e.destination==dest){
+    bool contains_edge(size_t src, size_t dest) const {
+        for (vertex * v : vertexList) {
+            for (edge t : v->connectedEdges) {
+                if (t.source = src && t.destination == dest) {
                     return true;
                 }
             }
         }
         return false;
     };
-
-    double cost(size_t src, size_t dest) const{
-        if(contains_edge(src,dest)==false){
+    double cost(size_t src, size_t dest) const {
+        if (contains_edge(src, dest) == false) {
             return INFINITY;
         }
-
         vertex* source;
-        for(auto& v : vertex_list){
+        for(auto& v : vertexList){
             if(v->id==src){
                 source = v;
             }
@@ -100,29 +119,26 @@ public:
                 w=e.weight;
             }
         }
-         return w;
+        return w;
+        
     };
 
-    // na
-    bool add_vertex(size_t id){
-        if (contains_vertex(id)==true){
+    bool add_vertex(size_t id) {
+        if (contains_vertex(id) == true) {
             return false;
         }
-
-        std::list<edge> newedge;
-        vertex* newVertex = new vertex(newedge, id);
-        vertex_list.push_back(newVertex);
+        std::list<edge> newEdge;
+        vertex * tempVertex = new vertex(newEdge, id);
+        vertexList.push_back(tempVertex);
         return true;
-
     };
-
-    bool add_edge(size_t src, size_t dest, double weight=1){
+    bool add_edge(size_t src, size_t dest, double weight=1) {
         if(contains_vertex(src)==false||contains_vertex(dest)==false||contains_edge(src,dest)==true){
             return false;
         }
 
         vertex* source;
-        for(auto& v : vertex_list){
+        for(auto& v : vertexList){
             if(v->id==src){
                 source = v;
             }
@@ -133,10 +149,48 @@ public:
         ++edgeCount;
         return true;
     };
-
-    //unni
-    bool remove_vertex(size_t id);
-    bool remove_edge(size_t src, size_t dest);
+    bool remove_vertex(size_t id) {
+        if (contains_vertex(id) == false) {
+            return false;
+        }
+        for (vertex * v : vertexList) {
+            remove_edge(v->id, id);
+        }
+        std::vector<vertex *>::iterator it = vertexList.begin();
+        while (it != vertexList.end()) {
+            if ((*it)->id == id) {
+                vertexList.erase(it);
+                return true;
+            }
+            else {
+                it++;
+            }
+        }
+        return false;
+    };
+    bool remove_edge(size_t src, size_t dest) {
+        if (contains_edge(src, dest) == false) {
+            return false;
+        }
+        vertex * tempVertex;
+        for (vertex * v : vertexList) {
+            if (v->id == src) {
+                tempVertex = v;
+            }
+        }
+        std::list<edge>::iterator it = tempVertex->connectedEdges.begin();
+        while (it != tempVertex->connectedEdges.end()) {
+            if (it->destination == dest) {
+                tempVertex->connectedEdges.erase(it);
+                --edgeCount;
+                return true;
+            }
+            else {
+                ++ it;
+            }
+        }
+        return false;
+    };
 
     // Task 2
     void prim(size_t source_id);
@@ -150,3 +204,22 @@ public:
 };
 
 #endif  // GRAPH_H
+
+
+// //erase function
+
+// std::unordered_map<size_t, Vertex*> map;
+// for (auto[v_id, v] : map) {
+//     if (v_id == id) {
+//         std::cout << v_id << std::endl;
+//     }
+// }
+
+// delete map[id];
+// map.erase(id);
+
+// //easiest if there is a map of map
+// std::unordered_map<size_t, Vertex*> vertices;
+                    
+// //                 src_id                    dest_id weight
+// std::unordered_map<size_t, std::unordered_map<size_t, double>> edges;
